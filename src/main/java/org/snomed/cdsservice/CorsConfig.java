d1c0dadfdb4066cd54a3a4680f157ce61c5adfa2
Harikrushna <h.g.vanpariya@gmail.com>
2025-05-21 11:05:29 +0530

    Added CORS

---
+++ src/main/java/org/snomed/cdsservice/CorsConfig.java
@@ -0,0 +1,19 @@
+package org.snomed.cdsservice;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/cds-services/**")
+                        .allowedOrigins("http://139.84.143.37:8080")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
