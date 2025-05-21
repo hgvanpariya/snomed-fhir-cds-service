540a67a9db36a863b19e40dab2c673a96e8e810d
Harikrushna <h.g.vanpariya@gmail.com>
2025-05-21 11:08:28 +0530

    Auto config

--- src/main/java/org/snomed/cdsservice/CdsServiceApplication.java
+++ src/main/java/org/snomed/cdsservice/CdsServiceApplication.java
@@ -1,9 +1,11 @@
 package org.snomed.cdsservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@AutoConfiguration
 public class CdsServiceApplication {
 
 	public static void main(String[] args) {
